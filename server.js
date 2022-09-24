f9d501fef97921799a0f0aa75a98d53bbabc6f95
Timothy-py <adeyeyetimothy33@gmail.com>
2022-09-25 00:56:03 +0100

    changed from smtp server to express server

--- server.js
+++ server.js
@@ -1,33 +1,69 @@
-const SMTPServer = require('smtp-server').SMTPServer;
-const config = require('./config.json')
+const express = require('express')
+const publisher = require('./publisher')
 
-const server = new SMTPServer({
-    logger: false,
-    banner: "Welcome to Mailer Microservice",
-    disabledCommands: ['STARTTLS'],
-    size: 10*1024*1024,
-    onAuth(auth, session, callback){
-        return callback(null, {
-            user: {
-                username: auth.username
-            }
-        })
-    },
-    onData(stream, session, callback){
-        console.log('Streaming message from user %s', session.user.username);
-        console.log('------------------');
-        stream.pipe(process.stdout)
-        stream.on('end', ()=>{
-            console.log('')
-            callback(null, 'Message queued as ' + Date.now())
-        })
+const app = express()
+
+app.use(express.json())
+
+
+app.post('/sendMsg', async(req, res) => {
+    const {subject, text} = req.body
+    const message = {
+        subject,
+        text
     }
+
+    recipients=[
+        "adeyeyetimothy33@gmail.com",
+        "tadeyeye@efficion.org",
+        "digitim33@gmail.com",
+        "atadeyeye@student.oauife.edu.ng",
+        "ayslim271@gmail.com"
+    ]
+
+    publisher(message, recipients)
+
+    res.send()
 })
 
-server.on('error', err => {
-    console.log('Error occurred');
-    console.log(err);
-});
+app.listen(3000, ()=>{
+    console.log("Server started....")
+})
 
-// start listening
-server.listen(config.server.port, config.server.host);
+
+
+
+// const SMTPServer = require('smtp-server').SMTPServer;
+// const config = require('./config.json')
+
+// const server = new SMTPServer({
+//     logger: false,
+//     banner: "Welcome to Mailer Microservice",
+//     disabledCommands: ['STARTTLS'],
+//     size: 10*1024*1024,
+//     onAuth(auth, session, callback){
+//         return callback(null, {
+//             user: {
+//                 username: auth.username
+//             }
+//         })
+//     },
+//     onData(stream, session, callback){
+//         console.log('Streaming message from user %s', session.user.username);
+//         console.log('------------------');
+//         stream.pipe(process.stdout)
+//         stream.on('end', ()=>{
+//             console.log('')
+//             callback(null, 'Message queued as ' + Date.now())
+//         })
+//     }
+// })
+
+// server.on('error', err => {
+//     console.log('Error occurred');
+//     console.log(err);
+// });
+
+// // start listening
+// server.listen(config.server.port, config.server.host);
+
